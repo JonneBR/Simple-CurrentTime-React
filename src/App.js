6277a40116153ba5ad1e60283c9713e5ca1b53a9
jonnebr <joaomgoncalvesbr@gmail.com>
2020-09-10 10:40:26 -0300

    minutes and seconds fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {
-    date: new Date(),
     hour: new Date(),
     minutes: new Date(),
     seconds: new Date(),
@@ -10,36 +9,35 @@ class App extends Component {
 
   callMe() {
     setInterval(() => {
-      this.setState({ date: new Date() });
+      this.setState({ hour: new Date() });
+      this.setState({ minutes: new Date() });
       this.setState({ seconds: new Date() });
     }, 1000);
   }
+
   render() {
-    // console.log(this.state.hour.getHours());
-    // console.log(this.state.minutes.getMinutes());
-    // console.log(this.state.seconds.getSeconds());
+    let minutes = this.state.minutes.getMinutes();
+    let seconds = this.state.minutes.getSeconds();
 
     return (
       <section className="timer-container">
-        <section className="timer">
-          <div className="title-container">
-            <h1>Current Time</h1>
-          </div>
+        <div className="title-container">
+          <h1>Current Time</h1>
+        </div>
 
-          <div className="divdoida">
-            <section>
-              <p>{this.state.hour.getHours()}</p>
-            </section>
-            <span>:</span>
-            <section>
-              <p>{this.state.minutes.getMinutes()}</p>
-            </section>
-            <span>:</span>
-            <section>
-              <p>{this.state.seconds.getSeconds()}</p>
-            </section>
-          </div>
-        </section>
+        <div className="divdoida">
+          <section>
+            <p>{this.state.hour.getHours()}</p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{minutes < 10 ? '0' + minutes : minutes}</p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{seconds < 10 ? '0' + seconds : seconds}</p>
+          </section>
+        </div>
         {this.callMe()}
       </section>
     );
